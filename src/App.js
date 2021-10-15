4edeb1a2df0ea5d3b944ac7c246774921e78600c
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2021-10-14 23:10:16 -0500

    Add Header, Footer components into App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
+  // obtener fecha
+  const fecha = new Date().getFullYear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Tienda Virtual" />
+      <Footer fecha={fecha} />
+    </Fragment>
   );
 }
 
